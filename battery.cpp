defa63225387afccfad834f67efbb26184738fbc
Manjunath K <91064711+ManjunathO7@users.noreply.github.com>
2024-10-10 15:28:06 +0530

    Update battery.cpp

--- battery.cpp
+++ battery.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 bool isMax(float value, float maxValue) { return value > maxValue; }
-
 bool isMin(float value, float minValue) { return value < minValue; }
+bool findLowThreshold(float value, float min, float tolerance){ return value > min&& value <= min + tolerance; }
+bool findHighThreshold(float value, float max, float tolerance) { return value >= max - tolerance && value < max; }
 
 void checkWarning(float value, float min, float max, const string& argument, string& message) {
     float tolerance = 0.05 * max;
-    bool low_threshold = value > min && value <= min + tolerance;
-    bool max_threshold = value >= max - tolerance && value < max;
+    bool low_threshold = findLowThreshold(value, min, tolerance);
+    bool max_threshold = findHighThreshold(value, max, tolerance);
     if (low_threshold) {
         message += "Warning: " + argument + " approaching discharge imit.\n";
     }
