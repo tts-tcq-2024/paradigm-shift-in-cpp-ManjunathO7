16a8c7fbd7efca94815adb356bae2d2865eaa0c5
Manjunath K <91064711+ManjunathO7@users.noreply.github.com>
2024-10-10 14:55:54 +0530

    Create battery.cpp

---
+++ battery.cpp
@@ -0,0 +1,47 @@
+#include "battery.h"
+#include <iostream>
+using namespace std;
+
+bool isMax(float value, float maxValue) { return value > maxValue; }
+
+bool isMin(float value, float minValue) { return value < minValue; }
+
+void checkWarning(float value, float min, float max, const string& argument, string& message) {
+    float tolerance = 0.05 * max;
+    bool low_threshold = value > min && value <= min + tolerance;
+    bool max_threshold = value >= max - tolerance && value < max;
+    if (low_threshold) {
+        message += "Warning: " + argument + " approaching discharge imit.\n";
+    }
+    else if (max_threshold) {
+        message += "Warning: " + argument + " approaching peak-charge limit.\n";
+    }
+}
+
+RangeResult isParametersinRange(float value, float min, float max, const string& argument) {
+    RangeResult result;
+    result.inRange = true;
+    result.message = "";
+    checkWarning(value, min, max, argument, result.message);
+    if (isMin(value, min)) {
+        result.message += argument + " too low.\n";
+        result.inRange = false;
+    }
+    else if (isMax(value, max)) {
+        result.message += argument + " too high.\n";
+        result.inRange = false;
+    }
+    return result;
+}
+
+RangeResult isChargeRateOk(float chargeRate) {
+    RangeResult result;
+    result.inRange = true;
+    result.message = "";
+    checkWarning(chargeRate, 0, 0.8, "Charge Rate ", result.message);
+    if (isMax(chargeRate, 0.8)) {
+        result.message += "Charge Rate out of range.\n";
+        result.inRange = false;
+    }
+    return result;
+}
