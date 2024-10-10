e225d2d8be5f29aca6d4d2e7d6ed8e1314a818f2
Manjunath K <91064711+ManjunathO7@users.noreply.github.com>
2024-10-10 15:16:50 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -2,8 +2,10 @@
 #define BATTERY_CHECK_H
 
 #include <string>
+#include "battery.h"
 using namespace std;
 
 bool batteryCheck(float temperature, float soc, float chargeRate);
+void printWarnings(const RangeResult& temperatureResult, const RangeResult& socResult, const RangeResult& chargeRateResult);
 
 #endif
